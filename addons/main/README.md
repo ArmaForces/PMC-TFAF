d27ee204ba2f9463a1073bcf6822b29e8d96e370
Filip Maciejewski <veteran29@users.noreply.github.com>
2021-01-03 21:12:40 +0100

    Initial commit

---
+++ addons/main/README.md
@@ -0,0 +1,7 @@
+## Main
+
+Base for other components. Provides CBA Macros framework and similar.
+
+### Authors
+
+- [veteran29](http://github.com/veteran29)
---
+++ mod.cpp
@@ -0,0 +1,16 @@
+name = "Task Force ArmaForces - 0.0.0";
+dir = "@armaforces_tfaf";
+author = "ArmaForces";
+
+url = "https://github.com/armaforces/tfaf/";
+action = "https://armaforces.com/";
+actionName = "Website";
+
+logo = "\z\tfaf\addons\main\ui\logo_ca.paa";
+logoOver = "\z\tfaf\addons\main\ui\logo_ca.paa";
+picture = "\z\tfaf\addons\main\ui\logo_ca.paa";
+overviewPicture = "\z\tfaf\addons\main\ui\logo_ca.paa";
+
+tooltip = "Task Force ArmaForces";
+description = "ArmaForces in-house mods developed by community members.";
+overview = "ArmaForces in-house mods developed by community members.";
